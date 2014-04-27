2f38a2d9f16a1e6c33f3e71aaf93f5c15ac2f295
Mattias Erming <mattias@mattiaserming.com>
2014-04-27 23:52:32 +0200

    Release 1.1.0

--- jquery.scrollGlue.js
+++ jquery.scrollGlue.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2014 Mattias Erming <mattias@mattiaserming.com>
  * Licensed under the MIT License.
  *
- * Version 1.0.1
+ * Version 1.1.0
  */
 (function($) {
 	$.fn.scrollGlue = function(options) {
