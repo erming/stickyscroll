c7627cbc0ca90ad97fc46305b92b3ee978b18034
Mattias Erming <mattias@mattiaserming.com>
2014-05-31 06:45:59 -0700

    Release 1.3.1

--- stickyscroll.js
+++ stickyscroll.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2014 Mattias Erming <mattias@mattiaserming.com>
  * Licensed under the MIT License.
  *
- * Version 1.3.0
+ * Version 1.3.1
  */
 (function($) {
 	$.fn.sticky = function(options) {
